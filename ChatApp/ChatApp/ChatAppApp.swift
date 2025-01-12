fb29ef7e9759a1de5dc2f92ab0c75a9e66857823
choala <pcy07111@gmail.com>
2025-01-12 20:42:23 +0900

    feat: 프로젝트 생성

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by 박채영 on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
