beb955d69953f70c96a7fac379a5c84ec3a9220f
choala <pcy07111@gmail.com>
2025-05-28 18:05:29 +0900

    Feat: AuthButton 생성

---
+++ ChatApp/ChatApp/View/Shared/AuthButton.swift
@@ -0,0 +1,18 @@
+//
+//  AuthButton.swift
+//  ChatApp
+//
+//  Created by 박채영 on 5/28/25.
+//
+
+import SwiftUI
+
+struct AuthButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthButton()
+}
