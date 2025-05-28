cbabf56d833c6751e0f44bad273494d636c44c51
choala <pcy07111@gmail.com>
2025-05-28 17:47:50 +0900

    Feat: 로그인, 회원가입 뷰 생성

---
+++ ChatApp/ChatApp/View/SignIn/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  ChatApp
+//
+//  Created by 박채영 on 5/28/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ ChatApp/ChatApp/View/SignUp/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  ChatApp
+//
+//  Created by 박채영 on 5/28/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
